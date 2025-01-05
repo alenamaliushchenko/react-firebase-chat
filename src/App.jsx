2440b48d3439d35459f84c1391a9d7f3c1d1de5a
Alena Maliushchenko <alenamaliushchenko@gmail.com>
2025-01-06 01:09:18 +0200

    chat-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import Chat from "./components/chat/Chat"
+import Detail from "./components/detail/Detail"
+import List from "./components/list/List"
+import ChatList from "./components/list/chatList/ChatList"
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className='container'>
+      <List/>
+      <Chat/>
+      <Detail/>
+      <ChatList/>
+    </div>
   )
 }
 
